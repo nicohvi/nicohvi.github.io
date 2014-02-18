2ac403e97ba9916ec8ccd49564a9610222504599
Nicolay Hvidsten <nico.hvi@gmail.com>
2014-02-18 17:32:53 +0100

    js media queries

--- javascript/main.js
+++ javascript/main.js
@@ -1,22 +1,41 @@
 $(document).ready(function() {
 
-	$('.post-container').each(function(index, element) {
+	var bindPosts = function() {
+		$('.post-container').each(function(index, element) {
+				$(element).on('webkitTransitionEnd', function(event) {
+					if( parseInt($(this).css('margin-left')) > 0 ) { return false }
+					$(this).siblings('.post-tags').addClass('active')
+				})
 
-		$(element).on('webkitTransitionEnd', function(event) {
-			if( parseInt($(this).css('margin-left')) > 0 ) { return false }
-			$(this).siblings('.post-tags').addClass('active')
-		})
+				$(element).on('mouseover', function(event) {
+					$(this).css('margin-left', 0)
+				})
 
-		$(element).on('mouseover', function(event) {
-			$(this).css('margin-left', 0)
-		})
+				$(element).on('mouseout', function(event) {
+					$(this).siblings('.post-tags').removeClass('active')
+					$(element).css('margin-left', $(this).siblings('.post-tags').css('width'))
+				})
 
-		$(element).on('mouseout', function(event) {
-			$(this).siblings('.post-tags').removeClass('active')
-			$(element).css('margin-left', $(this).siblings('.post-tags').css('width'))
-		})
+				$(element).css('margin-left', $(this).siblings('.post-tags').css('width'))
+			})
+	}
 
-		$(element).css('margin-left', $(this).siblings('.post-tags').css('width'))
-	})
+	var removeBindings = function(mediaQuery) {
+		if(mediaQuery.matches) {
+			$('.post-container').each(function(index, element) {
+				$(element).unbind()
+				$(element).css('margin-left', 0)
+			})
+		}
+		else {
+			bindPosts();
+		}
+	}
+
+	if(matchMedia) {
+		var mediaQuery = window.matchMedia("(max-width: 900px)")
+		mediaQuery.addListener(removeBindings)
+		removeBindings(mediaQuery)
+	}
 
 })
