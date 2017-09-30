00fee0200e6b9f83a065d419c9d721934a5dd36a
Nicolay Hvidsten <nicohvi@gmail.com>
2017-09-30 15:30:15 +0200

    whops

--- publish.sh
+++ publish.sh
@@ -3,9 +3,11 @@
 gulp build --NODE_ENV=production
 
 jekyll build
+# removes the html extension
+for f in `ls _site/blog/**/**/**/*.html`; do mv $f "${f%%.*}"; done
 
 s3cmd sync --default-mime-type="text/html; charset=utf-8" --guess-mime-type --delete-removed _site/ s3://nico.cool/
 
-rm -rf _site
+# rm -rf _site
 
 
