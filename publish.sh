d3d1300ac4b1f78cb3549c6435433fff65a6b474
Nicolay Hvidsten <nicohvi@gmail.com>
2017-09-30 15:13:41 +0200

    font cap

--- publish.sh
+++ publish.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-jekyll build
-
 gulp build --NODE_ENV=production
+
+jekyll build
 
 s3cmd sync --default-mime-type="text/html; charset=utf-8" --guess-mime-type --delete-removed _site/ s3://nico.cool/
 
