adf768133fc4932a33fbfb3d27782ab46b64cda2
Nicolay Hvidsten <nicohvi@gmail.com>
2017-09-30 13:15:11 +0200

    custom revision gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -1,55 +1,78 @@
 const gulp = require('gulp');
 const R = require('ramda');
 const b = require('browserify');
+const buffer = require('gulp-buffer');
 const w = require('watchify');
-const gutil = require('gulp-util');
+const { log, env, noop } = require('gulp-util');
 const sass = require('gulp-sass');
 const source = require('vinyl-source-stream');
-const min = require('gulp-imagemin');
+const rev = require('gulp-rev');
+const del = require('del');
+const through = require('through2');
+const fs = require('fs');
+const vFile = require('vinyl-file');
 
 // util
 
 function error (err) {
-  gutil.log(err.message);
+  log(err.message);
+}
+
+function manifest (file, enc, cb) {
+  const { path } = file;
+  let old = {};
+
+  const assetType = file.path.split('.').pop()
+
+  vFile.read('build/_data/assets.json')
+  .catch(err => log(err))
+  .then(manifest => {
+    try {
+      old = JSON.parse(manifest.contents.toString());
+    }
+    catch (err) { }
+
+    const contents = Object.assign(old, { [assetType]: path.split('/').pop() });
+    fs.writeFileSync('build/_data/assets.json', JSON.stringify(contents));
+    cb(null, file);
+  });
 }
 
 // functions
 
 function assets () {
-  gulp.src(['assets/static/**', '!assets/static/images'])
+  return gulp.src('assets/static/**')
   .pipe(gulp.dest('build/public'))
-
-  gulp.src('assets/static/images/**')
-  .pipe(min())
-  .pipe(gulp.dest('build/public'));
 }
 
-function css (watch = false) {
-  const prod = process.env.NODE_ENV === 'production';
+function css () {
+  const { NODE_ENV } = env;
+  const prod = NODE_ENV === 'production';
 
   function build () {
     const start = Date.now();
 
-    gutil.log("Transpiling CSS");
+    log("Transpiling CSS");
 
     const opts = {
       errLogToConsole: true,
       outputStyle: 'expanded'
     };
 
-    gulp.src('assets/css/style.scss')
+    return gulp.src('assets/css/style.scss')
     .pipe(sass(opts).on('error', error))
-    .pipe(gulp.dest('build/public'))
-    .on('end', () => gutil.log("CSS done"));
+    .pipe(prod ? gulp.dest('build/assets') : gulp.dest('build/public'))
+    .on('end', () => log("CSS done"));
   }
 
-  if(watch) gulp.watch('assets/css/**/*.scss', build);
+  if(!prod) gulp.watch('assets/css/**/*.scss', build);
 
-  build();
+  return build();
 }
 
-function js (watch = false) {
-  const prod = process.env.NODE_ENV === 'production';
+function js () {
+  const { NODE_ENV } = env;
+  const prod = NODE_ENV === 'production';
 
   const opts = {
     entries: ['assets/js/main.js'],
@@ -57,34 +80,42 @@ function js (watch = false) {
     transform: ['babelify']
   }
 
-  const bundler = watch ?
+  const bundler = !prod ?
   w(b(R.merge(opts, { cache: {}, packageCache: {} })))
   : b(opts);
 
   function build () {
     const start = Date.now();
 
-    gutil.log("Transpiling JS");
+    log("Transpiling JS");
 
-    bundler
+    return bundler
     .bundle()
     .on('error', error)
     .pipe(source('app.js'))
-    .pipe(gulp.dest('build/public'))
-    .on('end', () => gutil.log('JS done'));
+    .pipe(prod ? gulp.dest('build/assets') : gulp.dest('build/public'))
+    .on('end', () => log('JS done'));
   }
 
-  bundler.on('update', build);
-  build();
+  if(!prod) bundler.on('update', build);
+  return build();
+}
+
+function revision () {
+  return gulp.src(['build/assets/app.js', 'build/assets/style.css'])
+  .pipe(rev())
+  .pipe(gulp.dest('build/public'))
+  .pipe(through.obj(manifest))
+  .on('end', () => del('build/assets'));
 }
 
 // tasks
 
+// atomic
 gulp.task('assets', assets);
+gulp.task('css', css);
+gulp.task('js', js);
 
-gulp.task('watch', () => {
-  js(true);
-  css(true);
-});
-
-gulp.task('default', ['watch'])
+// task flows
+gulp.task('build', ['css', 'js'], revision);
+gulp.task('default', ['css', 'js']);
--- publish.sh
+++ publish.sh
@@ -2,10 +2,12 @@
 
 jekyll build
 
+NODE_ENV=production gulp build
+
 # removes the html extension
 for f in `ls _site/blog/**/**/**/*.html`; do mv $f "${f%%.*}"; done
 
-s3cmd sync --default-mime-type="text/html; charset=utf-8" --guess-mime-type --delete-removed _site/ s3://nico.cool/
+# s3cmd sync --default-mime-type="text/html; charset=utf-8" --guess-mime-type --delete-removed _site/ s3://nico.cool/
 
 # removes the generated site which has the .html and renamed files
 rm -rf _site
